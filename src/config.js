1e7fb4f10b22b4cc6fde68bf69aca37aeb91d9c5
Maxime Barbot <maxime.barbot@dalenys.com>
2016-10-07 09:59:54 +0200

    Setup up project

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const config =  {
+  port: process.env.PORT || 3000,
+  URL_ROOT: 'https://api.twitch.tv/kraken',
+  ROOT_URL: 'https://api.twitch.tv/kraken',
+  API_KEY: 'qx4xzkw0y73bt4mdp609z79oyvc31rz',
+};
+
+export default config;
